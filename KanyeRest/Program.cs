b1163cc641b4c660a87e86d06640d4eb0902c287
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-05-25 19:41:30 -0400

    both kanye and weather app exercise

---
+++ KanyeRest/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeRest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here's a random quote from Kanye");
+            
+            var url = "https://api.kanye.rest/";
+            var client = new HttpClient();
+            var response = client.GetStringAsync(url).Result;
+            var kanyeQuote = JObject.Parse(response).GetValue("quote").ToString();
+            Console.WriteLine(kanyeQuote);
+            Console.ReadLine();
+            Console.ReadKey();
+
+            Console.WriteLine("Now here's something from Ron Swanson");
+            Console.ReadKey();
+            
+
+            var ronURl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronClient = new HttpClient();
+            var ronsponse = ronClient.GetStringAsync(ronURl).Result;
+            
+            var ronQuote = JArray.Parse(ronsponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine(ronQuote);
+            Console.ReadKey();
+
+            Console.WriteLine("Now we're going to simulate a convo between the two of them by" +
+                " throwing these values in a forloop");
+            Console.ReadKey();
+
+           
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                response = client.GetStringAsync(url).Result;
+                kanyeQuote = JObject.Parse(response).GetValue("quote").ToString();
+                Console.WriteLine($"Kanye - {kanyeQuote}");
+                Console.WriteLine();
+                ronsponse = ronClient.GetStringAsync(ronURl).Result;
+                ronQuote = JArray.Parse(ronsponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                Console.WriteLine($"Ron - {ronQuote}");
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
---
+++ weatherapp/weatherapp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace weatherapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What city whose weather are you looking for?");
+            string ZIP = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Please enter your API key");
+            var api = Console.ReadLine();
+
+            var url = $"https://api.openweathermap.org/data/2.5/weather?zip={ZIP},us&appid={api}";
+
+
+            var client = new HttpClient();
+            var response = client.GetStringAsync(url).Result;
+            //Console.WriteLine(response);
+            var weatherInfo = JObject.Parse(response).GetValue("main").ToString();
+            var tempInfo = JObject.Parse(weatherInfo).GetValue("temp").ToString();
+
+            double kelvinInfo = Convert.ToDouble(tempInfo);
+
+            double convertFarenheit = kelvinInfo * 1.8 - 459.67;
+            Console.WriteLine("Currently Calculating.....");
+            Console.ReadKey();
+            Console.WriteLine($"At the moment, it's currently {convertFarenheit} degrees out!");
+        }
+    }
+}
